f6a99d53857c829d4e566556949f3afb7d297fc0
Walace <walaceleoric@gmail.com>
2026-05-18 11:42:18 -0300

    remover migration automatica

--- AssistenciaTecnica/Program.cs
+++ AssistenciaTecnica/Program.cs
@@ -31,17 +31,6 @@ builder.Services.AddSession();
 var app = builder.Build();
 
 
-// MIGRATION AUTOMÁTICA NO RENDER
-if (!app.Environment.IsDevelopment())
-{
-    using var scope = app.Services.CreateScope();
-
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
-    db.Database.Migrate();
-}
-
-
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
