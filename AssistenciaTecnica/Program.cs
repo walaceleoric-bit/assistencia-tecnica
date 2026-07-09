8378ad801d33aa7b4fa300ecf6ec3f3686a9b9b2
Walace <walaceleoric@gmail.com>
2026-07-09 19:27:06 -0300

    Agora vai

--- AssistenciaTecnica/Program.cs
+++ AssistenciaTecnica/Program.cs
@@ -19,7 +19,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+
+    // db.Database.Migrate();
 }
 
 if (!app.Environment.IsDevelopment())
