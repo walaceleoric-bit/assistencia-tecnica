6e14c43cb1a046c7060806d6ed6d42f96709034c
Walace <walaceleoric@gmail.com>
2026-07-16 12:24:22 -0300

    Correção de noov

--- AssistenciaTecnica/Controllers/HomeController.cs
+++ AssistenciaTecnica/Controllers/HomeController.cs
@@ -1,92 +1,37 @@
-using AssistenciaTecnica.Data;
 using AssistenciaTecnica.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace AssistenciaTecnica.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly AppDbContext _context;
-
-        public HomeController(AppDbContext context)
-        {
-            _context = context;
-        }
-
         public IActionResult Landing()
         {
-            HttpContext.Session.Clear();
-            return View();
+            return RedirectToAction("Index");
         }
 
-        private int EmpresaId()
+        public IActionResult Index()
         {
-            var empresaAdm = HttpContext.Session.GetInt32("EMPRESA_ID") ?? 0;
-
-            if (empresaAdm > 0)
-                return empresaAdm;
-
-            var empresaCliente = HttpContext.Session.GetInt32("CLIENTE_EMPRESA_ID") ?? 0;
-
-            if (empresaCliente > 0)
-                return empresaCliente;
-
-            return 0;
-        }
-
-        private async Task<Configuracao> ObterConfiguracaoDaEmpresa()
-        {
-            var empresaId = EmpresaId();
-
-            if (empresaId == 0)
+            return View(new Configuracao
             {
-                return new Configuracao
-                {
-                    NomeEmpresa = "Empresa",
-                    SubtituloEmpresa = "Assistência Técnica"
-                };
-            }
-
-            var config = await _context.Configuracoes
-                .FirstOrDefaultAsync(c => c.EmpresaId == empresaId);
-
-            if (config == null)
-            {
-                config = new Configuracao
-                {
-                    EmpresaId = empresaId,
-                    NomeEmpresa = "Empresa",
-                    SubtituloEmpresa = "Assistência Técnica",
-                    SenhaAdm = "123456"
-                };
-
-                _context.Configuracoes.Add(config);
-                await _context.SaveChangesAsync();
-            }
-
-            return config;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            var config = await ObterConfiguracaoDaEmpresa();
-            return View(config);
+                NomeEmpresa = "Teste",
+                SubtituloEmpresa = "Sistema funcionando sem banco"
+            });
         }
 
         public IActionResult Servicos()
         {
-            if (HttpContext.Session.GetString("CLIENTE_LOGADO") == "SIM")
-                return RedirectToAction("Index", "AreaCliente");
-
             return View();
         }
 
-        public async Task<IActionResult> Contato()
+        public IActionResult Contato()
         {
-            var config = await ObterConfiguracaoDaEmpresa();
-            return View(config);
+            return View(new Configuracao
+            {
+                NomeEmpresa = "Teste",
+                SubtituloEmpresa = "Sistema funcionando sem banco"
+            });
         }
 
         public IActionResult Privacy()
